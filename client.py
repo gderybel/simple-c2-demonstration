224ae1b13f80b146cc1fa47e2c945980a44e422b
Guillaume de Rybel <90607561+gderybel@users.noreply.github.com>
2025-07-24 15:28:45 +0200

    Update client.py to accept self signed certificates

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 """ Client module to connect to the socket Server"""
+import ssl
 import socket
 
 class SocketClient:
@@ -6,24 +7,26 @@ class SocketClient:
     def __init__(self, host='127.0.0.1', port=7053):
         self.host = host
         self.port = port
+        self.context = ssl._create_unverified_context()
         # Here, AF_INET corresponds to IPv4 (AF_INET6 for IPv6),
         # SOCK_STREAM to TCP (SOCK_DGRAM for UDP)
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.conn = self.context.wrap_socket(self.client_socket, server_hostname='localhost')
 
     def connect(self):
         """Connection to the defined server"""
-        self.client_socket.connect((self.host, self.port))
+        self.conn.connect((self.host, self.port))
         print(f"[*] Connected to server {self.host}:{self.port}")
 
     def send_message(self, message):
         """Send data to server"""
-        self.client_socket.send(message.encode())
-        response = self.client_socket.recv(1024).decode()
+        self.conn.send(message.encode())
+        response = self.conn.recv(1024).decode()
         return response
 
     def close(self):
         """Close socket connection"""
-        self.client_socket.close()
+        self.conn.close()
         print("[*] Connection closed")
 
 
